91b3964dd8e60d45a8feaa2e92df3a4d0b82a9ff
Mahdi-Mohamadiha <mahdi.mohamadiha.znu@gmail.com>
2025-04-22 20:20:33 +0330

    Add UUID to Ad model and update image upload path; modify URL patterns

--- ads/models.py
+++ ads/models.py
@@ -1,29 +1,35 @@
 from django.db import models
 from django.core.exceptions import ValidationError
 from os import path as osPath
+from uuid import uuid4
 
 
 def validate_image_extension(value):
     ext = osPath.splitext(value.name)[1].lower()
     valid_extensions = [".jpg", ".jpeg", ".png", ".gif"]
     if ext not in valid_extensions:
-        raise ValidationError("فرمت تصویر باید jpg، png یا gif باشد.")
+        raise ValidationError("فرمت تصویر باید jpg، jpeg، png یا gif باشد.")
 
 
 def ad_upload_path(instance, filename):
-    ad_id = str(instance.ad.id)
-    return f"ads/images/{ad_id} - {filename}"
+    return f"ads/images/{filename}_{instance.uuid}"
 
 
 class Ad(models.Model):
+    uuid = models.CharField(
+        max_length=8,
+        default=uuid4().hex[:8],
+        editable=False,
+        unique=True,
+    )
     image = models.ImageField(
         upload_to=ad_upload_path,
         validators=[validate_image_extension],
         verbose_name="تصویر تبلیغ",
     )
-    title = models.TextField(max_length=256, verbose_name="عنوان تبلیغ")
+    title = models.CharField(max_length=256, verbose_name="عنوان تبلیغ")
     link = models.URLField(verbose_name="لینک تبلیغ")
     description = models.TextField(max_length=256, verbose_name="توضیحات تبلیغ")
 
     def __str__(self):
-        return f"{self.title} ({self.id})"
+        return f"{self.title} - {self.description[:30]}"
--- adspanel/urls.py
+++ adspanel/urls.py
@@ -5,5 +5,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ads/', include("ads.urls")),
+    path('api/ads/', include("ads.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
