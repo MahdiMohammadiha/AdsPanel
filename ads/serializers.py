7d687f09086f7f2492e8bbc5665cffd7923ce592
Mahdi-Mohamadiha <mahdi.mohamadiha.znu@gmail.com>
2025-04-22 22:56:51 +0330

    Add AdMinimalSerializer and AdsByService view for filtered ad retrieval

--- ads/serializers.py
+++ ads/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from .models import Ad
 
+
 class AdSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ad
-        fields = '__all__'
+        fields = "__all__"
+
+
+class AdMinimalSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ad
+        fields = ["image", "link", "title", "description"]
--- ads/views.py
+++ ads/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework import generics
-from .models import Ad
-from .serializers import AdSerializer
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Ad, Service
+from .serializers import AdSerializer, AdMinimalSerializer
 
 
 def index(request):
@@ -12,6 +14,16 @@ class AdList(generics.ListAPIView):
     queryset = Ad.objects.all()
     serializer_class = AdSerializer
 
+
 class AdDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Ad.objects.all()
     serializer_class = AdSerializer
+
+
+class AdsByService(generics.ListAPIView):
+    serializer_class = AdMinimalSerializer
+
+    def get_queryset(self):
+        service_id = self.kwargs["service_id"]
+        service = get_object_or_404(Service, id=service_id)
+        return Ad.objects.filter(services=service, is_active=True)
