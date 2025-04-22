0d26b021c1fb74584c3ff65cb55302934dec4c99
Mahdi-Mohamadiha <mahdi.mohamadiha.znu@gmail.com>
2025-04-22 12:50:15 +0330

    Add API endpoints for Ad model with serialization

--- ads/urls.py
+++ ads/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import index
+from .views import index, AdList, AdDetail
 
 urlpatterns = [
     path("", index, name="index"),
+    path("api/ad/", AdList.as_view()),
+
+    # Read notes in todo.md
+    # path("api/ad/<int:pk>/", AdDetail.as_view()),  
 ]
--- ads/views.py
+++ ads/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Ad
+from .serializers import AdSerializer
 
-def index (request):
+
+def index(request):
     return render(request, "index.html")
+
+
+class AdList(generics.ListAPIView):
+    queryset = Ad.objects.all()
+    serializer_class = AdSerializer
+
+class AdDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Ad.objects.all()
+    serializer_class = AdSerializer
